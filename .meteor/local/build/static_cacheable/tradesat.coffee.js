796db4f7ea63e7635211dbfc22d4623743b30cf7
Bram den Teuling <bramdenteuling@gmail.com>
2013-04-10 18:43:55 +0200

    data kosovo

--- .meteor/local/build/static_cacheable/tradesat.coffee.js
+++ .meteor/local/build/static_cacheable/tradesat.coffee.js
@@ -1,7 +1,7 @@
 (function(){ 
 if (Meteor.isClient) {
   Meteor.startup(function() {
-    var colors, h, month_names, projection, redraw, svg, t, w, xport, xport_toggle_div,
+    var colors, h, month, month_invalid_date_fix, month_names, projection, redraw, svg, t, w, xport, xport_toggle_div,
       _this = this;
     w = 960;
     h = 540;
@@ -16,7 +16,7 @@ if (Meteor.isClient) {
         precision: 0
       }
     }, accounting.settings);
-    t = 2;
+    t = 0;
     xport = "import";
     this.tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
     this.line = d3.svg.line().interpolate("cardinal").x(function(d) {
@@ -38,7 +38,7 @@ if (Meteor.isClient) {
     projection = d3.geo.mercator().scale((w + 1) / 2 / Math.PI * 3.8).translate([w / 3, h * 1.73]).precision(.1);
     this.tradecolor = function(data, date) {
       var ramp;
-      ramp = d3.scale.log().domain([10, 10000000]).range(["white", "blue"]);
+      ramp = d3.scale.log().domain([10, 5000000]).range(["white", "#08306b"]);
       if (data) {
         if (xport === "export") {
           return ramp(data["export"][date]);
@@ -52,32 +52,69 @@ if (Meteor.isClient) {
     this.cash = function(data) {
       var export_cash, import_cash;
       if (data) {
-        export_cash = data["export"]["2012-" + t] * 1000;
-        import_cash = data["import"]["2012-" + t] * 1000;
+        export_cash = data["export"][month(t)] * 1000;
+        import_cash = data["import"][month(t)] * 1000;
         return "<h5>          Export: " + (accounting.formatMoney(export_cash)) + "        </h5>        <h5>          Import: " + (accounting.formatMoney(import_cash)) + "        </h5>";
       } else {
         return "";
       }
     };
-    svg = d3.select(".span12").append("div").attr("class", "chart-container").append("svg").attr("class", "chart").attr("width", w).attr("height", h);
+    month = function(t) {
+      t = parseInt(t);
+      if (t < 6) {
+        return "2011-" + (t + 7);
+      } else {
+        return "2012-" + (t - 5);
+      }
+    };
+    month_invalid_date_fix = function(t) {
+      t = parseInt(t);
+      if (t < 6) {
+        if (t + 7 < 10) {
+          return "2011-0" + (t + 7);
+        } else {
+          return "2011-" + (t + 7);
+        }
+      } else {
+        if (t - 5 < 10) {
+          return "2012-0" + (t - 5);
+        } else {
+          return "2012-" + (t - 5);
+        }
+      }
+    };
+    svg = d3.select(".chart-span").append("div").attr("class", "chart-container").append("svg").attr("class", "chart").attr("width", w).attr("height", h);
     this.metric = d3.select(".chart-container").append("div").attr("class", "metric");
-    this.date = d3.select(".chart-container").append("div").attr("class", "date").append("h3").html("Januari 2012");
+    this.date = d3.select(".chart-container").append("div").attr("class", "date").append("h3").html("Juli 2011");
     svg.selectAll("path").data(geoData.features).enter().append("path").attr("class", "country").attr("id", function(d) {
       return d.properties.iso_a3;
     }).attr("d", d3.geo.path().projection(projection)).style("fill", function(d) {
-      return tradecolor(data[d.properties.iso_a3], "2012-" + t);
+      return tradecolor(data[d.properties.iso_a3], month(t));
     }).on("mouseover", function(d, i) {
       var _this = this;
-      d3.select(this).transition().duration(300).style('stroke', 'red');
+      svg.selectAll("path").sort(function(a, b) {
+        if (a.properties.iso_a3 !== d.properties.iso_a3) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      d3.select(this).transition().duration(0).style('stroke', '#aa0000').style("stroke-width", 2);
       return metric.style("opacity", 1).html(function() {
         return "<h3>" + d.properties.name + "</h3>" + (cash(data[d.properties.iso_a3]));
       });
     }).on("mouseout", function(d, i) {
-      d3.select(this).transition().duration(300).style('stroke', "");
+      d3.select(this).transition().duration(200).style('stroke', "").style("stroke-width", 1);
       return metric.style("opacity", 0);
     });
-    d3.select(".chart-container").append("input").attr("type", "range").attr("min", 1).attr("max", 12).attr("value", 2).on("change", function() {
-      return redraw(this.value);
+    d3.select(".chart-container").append("div").attr("id", "slider");
+    $("#slider").slider({
+      min: 0,
+      max: 15,
+      value: 0,
+      slide: function(e, ui) {
+        return redraw(ui.value);
+      }
     });
     xport_toggle_div = d3.select(".chart-container").append("div").attr("class", "btn-group").attr("data-toggle", "buttons-radio");
     xport_toggle_div.append("button").attr("type", "button").attr("class", "btn active").text("Import").on("click", function() {
@@ -92,9 +129,10 @@ if (Meteor.isClient) {
       if (value) {
         t = value;
       }
-      date.html("" + month_names[t - 1] + " 2012");
-      return svg.selectAll("path").style("fill", function(d) {
-        return tradecolor(data[d.properties.iso_a3], "2012-" + t);
+      t = parseInt(t);
+      date.html("" + month_names[(new Date(month_invalid_date_fix(t))).getMonth()] + " " + ((new Date(month_invalid_date_fix(t))).getFullYear()));
+      return svg.selectAll("path").transition(100).style("fill", function(d) {
+        return tradecolor(data[d.properties.iso_a3], month(t));
       });
     };
   });
